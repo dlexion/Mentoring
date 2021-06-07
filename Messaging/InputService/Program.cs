52c85ae9eea7aa5dbdcc2c33a451d4c720477c07
Aliaksei Dubinevich <dlexion@yandex.ru>
2021-06-07 10:47:05 +0300

    Add initial message sending and receiving using rabbitmq

---
+++ Messaging/InputService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace InputService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("messaging",
+                    false,
+                    false,
+                    false,
+                    null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("",
+                    "messaging",
+                    null,
+                    body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
