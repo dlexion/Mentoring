34a9a5c078820e1a268da5ae06bb920d243cc550
Aliaksei Dubinevich <dlexion@yandex.ru>
2021-07-18 21:38:02 +0300

    Initial Composite pattern task

---
+++ Patterns/Task (Composite)/Composite.Task2/Form.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Composite.Task2
+{
+    public class Form : IComponent
+    {
+        String name;
+
+        public Form(String name)
+        {
+            this.name = name;
+        }
+
+        public void AddComponent(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToString(int depth = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Patterns/Task (Composite)/Composite.Task2/InputText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Composite.Task2
+{
+    public class InputText : IComponent
+    {
+        string name;
+        string value;
+
+        public InputText(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public string ConvertToString(int depth = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
